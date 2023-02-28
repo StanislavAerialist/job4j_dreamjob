bd0576febb72a7a43f258863913251f662a047e9
StanislavAerialist <moonlight_stas@mail.ru>
2023-02-28 20:44:20 +0500

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -8,11 +8,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private int nextId = 1;
+    private final AtomicInteger nextId = new AtomicInteger(1);
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
     private MemoryCandidateRepository() {
@@ -26,9 +27,9 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(nextId++);
+        candidate.setId(nextId.incrementAndGet());
         candidates.put(candidate.getId(), candidate);
-        return null;
+        return candidate;
     }
 
     @Override
